0077767d52c9a892e6e1afc6bae978455816d5a3
Yassin Ibrahim <yassin.ibrahim.hamed@gmail.com>
2025-05-04 16:45:43 +0300

    sitemap

--- next-sitemap.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-
-module.exports = {
-  siteUrl: "https://yassin.icu/",
-  generateRobotsTxt: true,
-  changefreq: "daily",
-  priority: 0.7,
-};
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,43 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains;",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "no-referrer-when-downgrade",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "geolocation=(), microphone=(), camera=()",
+          },
+          {
+            key: "Upgrade-Insecure-Requests",
+            value: "1",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
